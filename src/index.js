82ca5e962a12d0197da39c217f649d224cea6789
SKamesiepo <skamesiepo@gmail.com>
2024-09-25 18:48:34 +0200

    Fixed Dependency errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+// Import React and ReactDOM correctly for React 17
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Use react-dom, not react-dom/client
 import './index.css';
 import App from './App';
 
-// Patch to fix passive event listeners
+// Patch to fix passive event listeners (your existing code)
 (function() {
   let supportsPassive = false;
   try {
@@ -28,14 +29,10 @@ import App from './App';
   };
 })();
 
-// Render the React app
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Render the React app using ReactDOM.render for React 17
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
